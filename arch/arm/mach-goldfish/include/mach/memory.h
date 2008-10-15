8896d50b438973a9926e5434d2b742c3176afae9
Mike A. Chan <mikechan@google.com>
2008-10-14 19:46:11 -0700

    goldfish: Add goldfish platform.
    Change-Id: Iafb853c3496f53a7b18e11e3ec8284382b55e0f7
    Signed-off-by: Tom Keel <thomas.keel@intel.com>
    Signed-off-by: keunyoung <keunyoung@google.com>

---
+++ arch/arm/mach-goldfish/include/mach/memory.h
@@ -0,0 +1,35 @@
+/* include/asm-arm/arch-goldfish/memory.h
+**
+** Copyright (C) 2007 Google, Inc.
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+*/
+
+#ifndef __ASM_ARCH_MEMORY_H
+#define __ASM_ARCH_MEMORY_H
+
+/*
+ * Physical DRAM offset.
+ */
+#define PHYS_OFFSET	UL(0x00000000)
+#define BUS_OFFSET	UL(0x00000000)
+
+/*
+ * Virtual view <-> DMA view memory address translations
+ * virt_to_bus: Used to translate the virtual address to an
+ *              address suitable to be passed to set_dma_addr
+ * bus_to_virt: Used to convert an address for DMA operations
+ *              to an address that the kernel can use.
+ */
+#define __virt_to_bus(x)	(x - PAGE_OFFSET + BUS_OFFSET)
+#define __bus_to_virt(x)	(x - BUS_OFFSET + PAGE_OFFSET)
+
+#endif
