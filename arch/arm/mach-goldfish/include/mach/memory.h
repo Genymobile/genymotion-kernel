d7982fd86405ca83801edc36a6ea3b8e8e6d5c57
David 'Digit' Turner <digit@google.com>
2010-01-28 10:40:58 -0800

    goldfish: increase framebuffer aperture size
    This is needed to support higher resolutions (e.g. 1024x768)
    in the Android emulator.
    
    Change-Id: Ief3807e3c8b2d67383b3a9af24865b79662e51be
    Signed-off-by: Tom Keel <thomas.keel@intel.com>

--- arch/arm/mach-goldfish/include/mach/memory.h
+++ arch/arm/mach-goldfish/include/mach/memory.h
@@ -23,6 +23,21 @@
 #define BUS_OFFSET	UL(0x00000000)
 
 /*
+ * Needed to allow framebuffer sizes > 1MB.
+ * This must be a multiple of SZ_2M.
+ *
+ * Note that using 4MB allows us to use
+ * framebuffers of up to 2 MB each (two are
+ * used for normal operations).
+ *
+ * For some reason, using more than 4MB here
+ * doesn't allow our emulated system to use
+ * larger framebuffers.
+ *
+ */
+#define CONSISTENT_DMA_SIZE      (2*SZ_2M)
+
+/*
  * Virtual view <-> DMA view memory address translations
  * virt_to_bus: Used to translate the virtual address to an
  *              address suitable to be passed to set_dma_addr
