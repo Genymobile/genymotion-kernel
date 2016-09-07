f8878b03b74f4494f3a8dca91b97d156a3a55856
Chih-Wei Huang <cwhuang@linux.org.tw>
2016-09-07 14:58:46 +0800

    Revert "mmc: sdhci-acpi: Fix device hang on Intel BayTrail"
    This reverts commit 3d170c1f5a915e0fbb799d13cadeb6c7f159c24a.
    
    Conflicts:
    	drivers/mmc/host/sdhci-acpi.c

--- drivers/mmc/host/sdhci-acpi.c
+++ drivers/mmc/host/sdhci-acpi.c
@@ -43,24 +43,6 @@
 
 #include "sdhci.h"
 
-#ifdef CONFIG_X86
-#include <asm/cpu_device_id.h>
-static bool sdhci_acpi_on_byt(void)
-{
-	static const struct x86_cpu_id byt[] = {
-		{ X86_VENDOR_INTEL, 6, 0x37 },
-		{}
-	};
-
-	return x86_match_cpu(byt);
-}
-#else
-static bool sdhci_acpi_on_byt(void)
-{
-	return false;
-}
-#endif
-
 enum {
 	SDHCI_ACPI_SD_CD		= BIT(0),
 	SDHCI_ACPI_RUNTIME_PM		= BIT(1),
@@ -164,14 +146,6 @@ static const struct sdhci_acpi_chip sdhci_acpi_chip_int = {
 	.ops = &sdhci_acpi_ops_int,
 };
 
-static void sdhci_acpi_int_dma_latency(struct sdhci_host *host)
-{
-	if (sdhci_acpi_on_byt()) {
-		host->dma_latency = 20;
-		host->lat_cancel_delay = 275;
-	}
-}
-
 static int bxt_get_cd(struct mmc_host *mmc)
 {
 	int gpio_cd = mmc_gpio_get_cd(mmc);
@@ -217,8 +191,6 @@ static int sdhci_acpi_emmc_probe_slot(struct platform_device *pdev,
 	    sdhci_readl(host, SDHCI_CAPABILITIES_1) == 0x00000807)
 		host->timeout_clk = 1000; /* 1000 kHz i.e. 1 MHz */
 
-	sdhci_acpi_int_dma_latency(host);
-
 	return 0;
 }
 
@@ -232,8 +204,6 @@ static int sdhci_acpi_sdio_probe_slot(struct platform_device *pdev,
 		return 0;
 
 	host = c->host;
-
-	sdhci_acpi_int_dma_latency(host);
 
 	/* Platform specific code during sdio probe slot goes here */
 
@@ -250,8 +220,6 @@ static int sdhci_acpi_sd_probe_slot(struct platform_device *pdev,
 		return 0;
 
 	host = c->host;
-
-	sdhci_acpi_int_dma_latency(host);
 
 	/* Platform specific code during sd probe slot goes here */
 
